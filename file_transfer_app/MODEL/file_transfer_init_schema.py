3a18d47dbba32de16983105578091758575b6468
colefromportland <colefromportland@gmail.com>
2019-06-19 14:42:56 -0600

    redesign of SQLite schema

--- file_transfer_app/MODEL/file_transfer_init_schema.py
+++ file_transfer_app/MODEL/file_transfer_init_schema.py
@@ -1,5 +1,6 @@
 import sqlite3 as sql
 
+
 # create database in SQLite3
 def createDb():
     conn = sql.connect('file_transfer.db')
@@ -9,25 +10,23 @@ def createDb():
 # init SQLite3 db objects
 def initSchema(c):
     # utilizing DROP / CREATE for unit testing
-    c.execute("DROP TABLE IF EXISTS Program")
-    c.execute("CREATE TABLE Program (ProgramID INT not null,"
-              "ProgramName TEXT not null,"
-              "TransferDate TEXT not null,"
-              "TransferID INT not null,"
-              "FOREIGN KEY (TransferID) REFERENCES Transfer(TransferID))")
+    c.execute("DROP TABLE IF EXISTS program")
+    c.execute("CREATE TABLE program (programId INT not null,"
+              "programName TEXT not null,"
+              "transDate TEXT not null,"
+              "tranId INT not null,"
+              "FOREIGN KEY (tranId) REFERENCES file_transfer(tranId))")
 
-    c.execute("DROP TABLE IF EXISTS Transfers")
-    c.execute("CREATE TABLE Transfers (TransferID INTEGER PRIMARY KEY AUTOINCREMENT,"
-              "RecentModification TEXT not null,"
-              "TransferDate TEXT not null,"
-              "Comments TEXT null,"
-              "FOREIGN KEY (RecentModification) REFERENCES File(ModifiedDate))")
+    c.execute("DROP TABLE IF EXISTS file_transfer")
+    c.execute("CREATE TABLE file_transfer (tranId INTEGER PRIMARY KEY AUTOINCREMENT,"
+              "recentMods TEXT not null,"
+              "transDate TEXT not null,"
+              "comments TEXT null)")
 
-    c.execute("DROP TABLE IF EXISTS FileInfo")
-    c.execute("CREATE TABLE FileInfo (FileID INTEGER PRIMARY KEY AUTOINCREMENT, "
-              "Path VARCHAR(80) not null,"
-              "NewPath VARCHAR(80) not null,"
-              "Type VARCHAR(20) not null,"
-              "TransferID INT not null,"
-              "FOREIGN KEY (Path) REFERENCES FileInfo(Path) "
-              "FOREIGN KEY (TransferID) REFERENCES Transfer(TransferID))")
+    c.execute("DROP TABLE IF EXISTS file_info")
+    c.execute("CREATE TABLE file_info (fileId INTEGER PRIMARY KEY AUTOINCREMENT, "
+              "root VARCHAR(80) not null,"
+              "dest VARCHAR(80) not null,"
+              "fileType VARCHAR(20) not null,"
+              "tranId INT not null,"
+              "FOREIGN KEY (tranId) REFERENCES file_transfer(tranId))")
